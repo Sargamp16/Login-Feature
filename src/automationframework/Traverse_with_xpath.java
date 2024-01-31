299fcbd67d2da003c1c195e43aee0c812fe54ebe
sargam <sargampanwar02@gmail.com>
2024-01-31 16:31:13 +0530

    Traverse using xpath locator

---
+++ src/automationframework/Traverse_with_xpath.java
@@ -0,0 +1,15 @@
+package automationframework;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Traverse_with_xpath {
+	public static void main(String args[])
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		driver.close();
+	}
+
+}
